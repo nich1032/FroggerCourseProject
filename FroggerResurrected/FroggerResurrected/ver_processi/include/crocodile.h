8b9bd2819b21fad3da4efc15cf4d201d4cb4a0b0
nich1032 <n.fadda10@studenti.unica.it>
2025-09-09 17:58:15 +0200

    Delete FroggerResurrected directory

--- FroggerResurrected/FroggerResurrected/ver_processi/include/crocodile.h
+++
@@ -1,8 +0,0 @@
-
-#include "../include/game.h" // Include the header file that defines struct position
-
-void coccodrillo(int pipeout,int id);
-
-static void handle_border_collision(struct position *p, int *original_width);
-
-static void update_position(struct position *p, int direction);
--- FroggerResurrected/FroggerResurrected/ver_processi/include/player.h
+++
@@ -1,5 +0,0 @@
-#include "game.h"
-#include <ncurses.h>
-
-
-void rana(int);
